f87a43e95f0567fd121acbd719ac5a204ecc4b87
Daichi Kamemoto <daikame@gmail.com>
2017-03-12 18:02:09 +0900

    Initial Commit

---
+++ MetalCNNImageRecognizerSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MetalCNNImageRecognizerSample
+//
+//  Created by yudoufu on 2017/03/12.
+//  Copyright © 2017年 Personal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
